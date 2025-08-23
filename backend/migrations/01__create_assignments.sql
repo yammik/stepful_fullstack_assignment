022514a058ace504125019899c05b9451726881d
yammik <23164118+yammik@users.noreply.github.com>
2025-08-23 17:14:02 -0400

    Update wording for consistency

--- backend/migrations/01__create_assignments.sql
+++ backend/migrations/01__create_assignments.sql
@@ -1,15 +1,15 @@
 CREATE TABLE
-  assignments (
+  quizzes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
   );
 
 CREATE TABLE
-  assignment_questions (
+  quiz_questions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    assignment_id INTEGER NOT NULL REFERENCES assignments (id),
-    question_content TEXT NOT NULL,
+    quiz_id INTEGER NOT NULL REFERENCES quizzes (id),
+    question_content TEXT,
     choices TEXT, -- sqlite doesn't support arrays, so we'll store choices as a string of ';;'-separated values
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
   );
