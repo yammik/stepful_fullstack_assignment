0bef9cf26346dcfd54f82728673c16e024c4f997
yammik <23164118+yammik@users.noreply.github.com>
2025-08-24 15:27:01 -0400

    sketch out quiz-results.tsx

--- frontend/src/components/quiz-results.tsx
+++ frontend/src/components/quiz-results.tsx
@@ -1,5 +1,25 @@
-export function QuizResults() {
-	// on mount, get questions and refetch attempt
-	// on unmount, update attempt
-	return <></>;
+import type { Attempt } from "./attempt";
+
+interface QuizResultsProps {
+	attempt: Attempt;
+}
+export function QuizResults({ attempt }: QuizResultsProps) {
+	/**
+	 * TODO: A more helpful results view.
+	 * - User should see their selected answers (requires answer_key)
+	 * - User should see the correct answer for each question, i.e. did they get it right?
+	 * - User should see feedback for each question
+	 * - User should see tallied up score (requires score property)
+	 * - (debatable) User should see how long it took to finish the quiz
+	 */
+	return (
+		<div>
+			You have finished.
+			<div>
+				{/* stats */}
+				<p>Begun at: {attempt.created_at}</p>
+				<p>Finished at: {attempt.updated_at}</p>
+			</div>
+		</div>
+	);
 }
